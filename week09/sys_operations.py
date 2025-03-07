c4f97609804241212f1e54f60f3775367f85391b
Karim <101488793@georgebrown.ca>
2025-03-07 15:34:01 -0500

    add lab9

---
+++ week09/sys_operations.py
@@ -0,0 +1,14 @@
+import platform
+import socket
+import os
+import sys
+
+print(f"\nMachine Type: {platform.machine()}")
+print(f"\nProcessor Type: {platform.architecture()}")
+
+socket.setdefaulttimeout(50)
+print(f"\nDefault Timeout for Socket: {socket.getdefaulttimeout()}")
+
+print(f"\nOS Type: {os.name}")
+print(f"\nOS Name: {platform.system()}")
+print(f"\nCurrent PID: {os.getpid()}")
